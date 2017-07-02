a1da0fababda15e5383f015fbc4593dd1635338a
Carllawrence <carl.lawrence@live.com>
2017-07-02 22:59:22 +0200

    updated

--- platforms/browser/www/scripts/controllers/messages.js
+++
@@ -1,5 +0,0 @@
-angular.module('starsApp').controller('MessagesController',
-  ['$scope', '$http', '$stateParams', '$location', '$state', 'AuthService',
-    function ($scope, $http, $stateParams, $location, $state, AuthService) {
-       
-    }])
