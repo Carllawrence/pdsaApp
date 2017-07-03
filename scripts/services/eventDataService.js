f68a21a3d656baaac746ba3cb7dcd178ac816661
Carllawrence <carl.lawrence@live.com>
2017-07-03 15:25:52 +0200

    with splashscreen plugin

--- scripts/services/eventDataService.js
+++
@@ -1,9 +0,0 @@
-angular.module('starsApp').factory('Event', function ($resource) {
-    return $resource('http://192.168.8.100/api/events/:eventId', {
-      eventId: '@_id'
-    }, {
-      update: {
-        method: 'PUT'
-      }
-    });
-  });
--- scripts/services/userCRUD.js
+++
@@ -1,9 +0,0 @@
-angular.module('starsApp').factory('User', function ($resource) {
-    return $resource('http://192.168.8.100/api/appusers/:userId', {
-      userId: '@_id'
-    }, {
-      update: {
-        method: 'PUT'
-      }
-    });
-  });
