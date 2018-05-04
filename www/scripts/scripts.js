f507e1cd23d91ddd393077d9cbe3af15f3b31142
Carllawrence <carl.lawrence@live.com>
2018-05-04 04:55:55 -0400

    Signed-off-by: Carllawrence <carl.lawrence@live.com>

--- www/scripts/scripts.js
+++ www/scripts/scripts.js
@@ -88,7 +88,7 @@ angular.module("yapp")
 
 angular.module("yapp")
 .controller("DashboardCtrl", ["$scope", "$state","$http","$rootScope","$firebaseObject","$timeout", function (r, t, h, rs, fireobj,timeout) { 
-  var ref = firebase.database().ref();
+  var ref = firebase.database().ref('/students/-LBeJ_4AU2FdMULhUYBK/data/1/');
 
      var obj = fireobj(ref); 
   
