49cae2fdf10d732435a83f38c6cc348835eea8b4
Carllawrence <carl.lawrence@live.com>
2018-05-04 02:13:12 -0400

    Signed-off-by: Carllawrence <carl.lawrence@live.com>

--- www/app.js
+++ www/app.js
@@ -195,6 +195,77 @@ app.controller('HomeCtrl', ['currentAuth','$scope','$timeout', '$mdSidenav', '$l
     );
   };
 
+
+  var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.getElementById('scan').addEventListener('click', this.scan, false);
+
+    },
+
+    onDeviceReady: function() {
+        app.receivedEvent('deviceready');
+    },
+
+    
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+
+        
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+      //  receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    },
+
+  
+
+    scan: function() {
+        console.log('scanning');
+        
+        var scanner = cordova.plugins.barcodeScanner;
+
+        scanner.scan( function (result) { 
+
+
+          $scope.dataToast('Scanned');
+
+           
+
+           console.log("Scanner result: \n" +
+                "text: " + result.text + "\n" +
+                "format: " + result.format + "\n" +
+                "cancelled: " + result.cancelled + "\n");
+            console.log(result);
+            /*
+            if (args.format == "QR_CODE") {
+                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+            }
+            */
+
+        }, function (error) { 
+            console.log("Scanning failed: ", error); 
+        } );
+    },
+
+
+
+};
+
+app.initialize();
+
+
+
+
  $scope.onClick = function (points, evt) {
    console.log(points, evt);
  };
